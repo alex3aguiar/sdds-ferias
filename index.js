4634df30c0ea545b65ae8162affceaa5b6b882b0
Luis <reescolando@gmail.com>
2019-10-29 14:37:55 -0300

    feat(args): adicionada validação de argumentos de linha de comando

--- index.js
+++ index.js
@@ -10,11 +10,55 @@ var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
 
 var imageCaption = `Faltam ${diffDays} para as férias `;
 var loadedImage;
-console.log(imageCaption )
 
-writeInImage();
+let name = 'sadasd';
+
+const argumentCommands = {
+    '-h': () => {
+        console.log("Comandos:\n-h: Exibe esse texto de ajuda\n-n [NOME]: Define o nome do arquivo de saída\n");
+        return false;
+    },
+    '-n': (args) => {
+        name = `${args.splice(0,1)}`;
+        return true;
+    }
+}
+
+function evaluateArg(args) {
+    if (args.length) {
+        // existem argumentos
+        const toBeEvaluated = args.splice(0,1)[0];
+        const run = argumentCommands[toBeEvaluated] ? argumentCommands[toBeEvaluated](args) : false;
+        if (!argumentCommands[toBeEvaluated]) {
+            console.log(`Argumento "${toBeEvaluated}" não reconhecido`);
+        }
+        return { run: run, args: args };
+    } else {
+        // sem argumentos, só executar o programa
+        return { run: true, args: args };
+    }
+}
+
+function main(args = process.argv.slice(2)) {
+    let run = true;
+    let evaluate = args.length;
+
+    while (evaluate) {
+        const evaluation = evaluateArg(args);
+
+        // se algum argumento, em qualquer posição, impede de rodar,
+        // o programa não deve rodar.
+        run = run ? evaluation.run : false;
+
+        evaluate = evaluation.args.length;
+    }
+
+    if (run)
+        writeInImage()
+}
 
 function writeInImage() {
+    console.log(imageCaption)
     Jimp.read(fileName)
         .then(function (image) {
             loadedImage = image;
@@ -23,9 +67,11 @@ function writeInImage() {
         .then(function (font) {
             console.log("foi");
             loadedImage.print(font, 50, 50, imageCaption)
-                .write(fileName+"sadasd.jpg");
+                .write(`./wallpappers/${name}.jpg`);
         })
         .catch(function (err) {
             console.error(err);
         });
 }
+
+main();
